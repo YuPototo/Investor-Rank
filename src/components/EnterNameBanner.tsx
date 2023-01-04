39adff286930ec070ed1486d9e59dc2bdc21f34c
Yu Qin <sirius.qinyu@gmail.com>
2023-01-04 13:24:19 +0800

    Design enter name banner

--- src/components/EnterNameBanner.tsx
+++ src/components/EnterNameBanner.tsx
@@ -13,14 +13,23 @@ const EnterNameBanner: React.FC = () => {
   if (hasName || isOnEnterNamePage) return <></>;
 
   return (
-    <div className="flex w-full items-center justify-center gap-4 bg-red-100 p-2 text-center">
-      <div>
-        This is a platform for real people. Please enter your name. Accounts
-        without name will be deleted in 7 days.
+    <div className="relative bg-red-300">
+      <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap items-center justify-center gap-4">
+          <div className="text-center">
+            Please provide your name. Accounts without names are deleted in 7
+            days.
+          </div>
+          <div className=" w-full flex-shrink-0 sm:mt-0 sm:w-auto">
+            <Link
+              href="/enterName"
+              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
+            >
+              Enter Name
+            </Link>
+          </div>
+        </div>
       </div>
-      <Link className="btn-primary" href="/enterName">
-        Enter Name
-      </Link>
     </div>
   );
 };
