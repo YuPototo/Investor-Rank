e9077a885d60d5a1818048bf4a5fa5e7f9f3d751
Yu Qin <sirius.qinyu@gmail.com>
2023-01-06 12:58:46 +0800

    Add several new assets in seed script

--- prisma/seed/assetEntity.ts
+++ prisma/seed/assetEntity.ts
@@ -20,6 +20,54 @@ const data = [
     symbol: "ETH",
     assetType: AssetType.CRYPTO,
   },
+  {
+    id: 4,
+    name: "Binance Coin",
+    symbol: "BNB",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 5,
+    name: "XRP",
+    symbol: "XRP",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 6,
+    name: "Dogecoin",
+    symbol: "DOGE",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 7,
+    name: "Cardano",
+    symbol: "ADA",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 8,
+    name: "Polygon",
+    symbol: "MATIC",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 9,
+    name: "Litecoin",
+    symbol: "LTC",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 10,
+    name: "Polkadot",
+    symbol: "DOT",
+    assetType: AssetType.CRYPTO,
+  },
+  {
+    id: 11,
+    name: "Solana",
+    symbol: "SOL",
+    assetType: AssetType.CRYPTO,
+  },
 ];
 
 export default data;
