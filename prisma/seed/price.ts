c2082e35561aa5563478a33dba1121f7525113e4
Yu Qin <sirius.qinyu@gmail.com>
2023-01-06 13:08:57 +0800

    change seed script

--- prisma/seed/price.ts
+++ prisma/seed/price.ts
@@ -2,47 +2,71 @@ export const assetEntityId = {
   USD: 1,
   BTC: 2,
   ETH: 3,
+  BNB: 4,
+  XRP: 5,
+  DOGE: 6,
+  ADA: 7,
+  MATIC: 8,
+  LTC: 9,
+  DOT: 10,
+  SOL: 11,
 };
 
-const usdPrices = [
+const data = [
   {
-    id: 1,
     assetEntityId: assetEntityId.USD,
     price: 1,
     priceTimeId: 1,
   },
-];
-
-const btcPrices = [
   {
-    id: 3,
     assetEntityId: assetEntityId.BTC,
-    price: 10000,
+    price: 16817.42,
     priceTimeId: 1,
   },
   {
-    id: 4,
-    assetEntityId: assetEntityId.BTC,
-    price: 20000,
-    priceTimeId: 2,
-  },
-];
-
-const ethPrices = [
-  {
-    id: 5,
     assetEntityId: assetEntityId.ETH,
-    price: 1000,
+    price: 1249.67,
     priceTimeId: 1,
   },
   {
-    id: 6,
-    assetEntityId: assetEntityId.ETH,
-    price: 2000,
-    priceTimeId: 2,
+    assetEntityId: assetEntityId.BNB,
+    price: 257.16,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.XRP,
+    price: 0.34,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.DOGE,
+    price: 0.07,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.ADA,
+    price: 0.27,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.MATIC,
+    price: 0.78,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.LTC,
+    price: 74.2,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.DOT,
+    price: 4.62,
+    priceTimeId: 1,
+  },
+  {
+    assetEntityId: assetEntityId.SOL,
+    price: 13.17,
+    priceTimeId: 1,
   },
 ];
-
-const data = [...usdPrices, ...btcPrices, ...ethPrices];
-
 export default data;
--- prisma/seed/priceTime.ts
+++ prisma/seed/priceTime.ts
@@ -1,9 +1,5 @@
 const data = [
   {
-    // 5 mins ago
-    timestamp: new Date(Date.now() - 5 * 60 * 1000),
-  },
-  {
     timestamp: new Date(),
   },
 ];
