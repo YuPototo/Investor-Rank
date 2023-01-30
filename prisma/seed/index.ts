2bc38d82085fcc09a14827475c2b6cb9af199512
Yu Qin <sirius.qinyu@gmail.com>
2023-01-30 14:43:52 +0800

    Create 100 users when seeding db

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import assetEntityData from "./assetEntity";
-import userData from "./user";
+import users from "./user";
 import userAssets from "./userAsset";
 import parameter from "./parameter";
 import rank from "./rank";
@@ -9,9 +9,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.assetEntity.createMany({ data: assetEntityData });
-  await prisma.user.createMany({ data: userData.users });
-  await prisma.account.create({ data: userData.account });
-  await prisma.session.create({ data: userData.session });
+  await prisma.user.createMany({ data: users });
   await prisma.userAsset.createMany({ data: userAssets });
   await prisma.parameter.createMany({ data: parameter });
   await prisma.rank.createMany({ data: rank });
--- prisma/seed/user.ts
+++ prisma/seed/user.ts
@@ -1,120 +1,20 @@
 import type { User } from "@prisma/client";
 
-export const users: Omit<User, "createdAt">[] = [
-  {
-    id: "clbvp8fu400009k5j6y09bt7e",
-    email: "sirius.qinyu@gmail.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Yu",
-    familyName: "Qin",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt73",
-    email: "Leo@Messi.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Leo",
-    familyName: "Messi",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt11",
-    email: "Marcus@Rashford.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Marcus",
-    familyName: "Rashford",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt12",
-    email: "Lisandro@Martínez.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Lisandro",
-    familyName: "Martínez",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt13",
-    email: "Bruno@Fernandes.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Bruno",
-    familyName: "Fernandes",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt14",
-    email: "Lee@Bruce.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Lee",
-    familyName: "Bruce",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt15",
-    email: "Alex@Ferguson.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Alex",
-    familyName: "Ferguson",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt16",
-    email: "Paul@Graham.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Paul",
-    familyName: "Graham",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt17",
-    email: "warren@buffett.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Warren",
-    familyName: "Buffett",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt18",
-    email: "Harry@Maguire.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Harry",
-    familyName: "Maguire",
-  },
-  {
-    id: "clbvp8fu400009k5j6y09bt19",
-    email: "Jadon@Sancho.com",
-    emailVerified: null,
-    image: "https://cdn.discordapp.com/embed/avatars/0.png",
-    firstName: "Jadon",
-    familyName: "Sancho",
-  },
-];
+import { faker } from "@faker-js/faker";
 
-const account = {
-  userId: "clbvp8fu400009k5j6y09bt7e",
-  type: "oauth",
-  provider: "discord",
-  providerAccountId: "412075777828323350",
-  refresh_token: "hVPTv0frRpF1dOG30zbv2uX3FR7MBN",
-  access_token: "ipLmlq0nCZg8sKJcycFmrfG5QiOSmr",
-  expires_at: 1672113686,
-  token_type: "Bearer",
-  scope: "email identify",
-  id_token: null,
-  session_state: null,
-};
+// create a fake user
+function createUser(): Omit<User, "createdAt"> {
+  return {
+    id: faker.datatype.uuid(),
+    email: faker.internet.email(),
+    emailVerified: null,
+    image: faker.internet.avatar(),
+    firstName: faker.name.firstName(),
+    familyName: faker.name.lastName(),
+  };
+}
 
-const session = {
-  sessionToken: "6c08679e-74a4-4b57-b3f9-6ba567d78589",
-  userId: "clbvp8fu400009k5j6y09bt7e",
-  expires: new Date("2023-01-19 04:01:27.374"),
-};
+// create 100 fake users
+const users = Array.from({ length: 100 }, createUser);
 
-const data = {
-  users,
-  session,
-  account,
-};
-
-export default data;
+export default users;
