9383d7d94d8041c19d3a9e52089f6a8e403b845e
Yu Qin <sirius.qinyu@gmail.com>
2023-02-07 14:09:44 +0800

    Add test setup for integration test

--- src/server/trpc/router/tests/user.test.ts
+++ src/server/trpc/router/tests/user.test.ts
@@ -1,6 +1,20 @@
 import { createContextInner } from "../../context";
 import { appRouter } from "../_app";
-import { test, expect } from "vitest";
+import { test, expect, beforeAll, afterAll } from "vitest";
+import { prisma } from "../../../db/client";
+
+beforeAll(async () => {
+  await prisma.user.create({
+    data: {
+      uniqueName: "test_test",
+      email: "test@test.com",
+    },
+  });
+});
+
+afterAll(async () => {
+  await prisma.user.deleteMany();
+});
 
 test("add and get post", async () => {
   const ctx = await createContextInner({ session: null });
